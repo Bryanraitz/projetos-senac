d70f91bbfd8fa3ee6442ded17544a72ca8a4881c
Bryan <110053484+Bryanraitz@users.noreply.github.com>
2022-07-27 16:52:07 -0300

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,7 @@
+public class ex2 {
+    public static void main(String[] args) {
+        System.out.println("Bryan Raitz Oechsler");
+        System.out.println("Rua papa joao XXIII");
+        System.out.println("89160000-47988925107");
+    }
+}
