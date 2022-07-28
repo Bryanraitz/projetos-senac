d4d027aa3ccdd94cc8dc8b546b6a0cb59d89b39e
Bryan <110053484+Bryanraitz@users.noreply.github.com>
2022-07-28 16:28:58 -0300

    Add files via upload

---
+++ ex6.java
@@ -0,0 +1,9 @@
+public class ex6 {
+    public static void main(String[] args) {
+        System.out.println("XXXXX");
+        System.out.println("X   X");
+        System.out.println("X   X");
+        System.out.println("X   X");
+        System.out.println("XXXXX");
+    }
+}
