c60d20919e2f8b937821fa8e9107f2a357d1022e
Bryan <110053484+Bryanraitz@users.noreply.github.com>
2022-08-01 14:12:10 -0300

    Delete ex2.java

--- ex2.java
+++
@@ -1,7 +0,0 @@
-public class ex2 {
-    public static void main(String[] args) {
-        System.out.println("Bryan Raitz Oechsler");
-        System.out.println("Rua papa joao XXIII");
-        System.out.println("89160000-47988925107");
-    }
-}
