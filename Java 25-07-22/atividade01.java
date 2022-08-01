111e45289532e157bb7b5492f1b41ea1273109b2
Bryan <110053484+Bryanraitz@users.noreply.github.com>
2022-08-01 16:03:03 -0300

    Add files via upload

---
+++ Java 25-07-22/atividade01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class atividade01 {
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        int a,b,c;
+
+        System.out.printf(" Escreva os valores de A e B");
+        a= teclado.nextInt();
+        b= teclado.nextInt();
+        c= teclado.nextInt();
+
+        if (a+b<c){
+            System.out.println("a soma de A+B é menor que C");
+        }
+        if (a+b>c){
+            System.out.println("a soma de A+B e maior que C");
+        }
+        if (a+b==c){
+            System.out.println("a soma de A+B e igual a C");
+        }
+
+    }
+}
