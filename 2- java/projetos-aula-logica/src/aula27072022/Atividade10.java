0b5dbdf8bec04ddac1b78a7f802b5f20a6df1821
bryan.oechsler <bryanraitz598@hotmail.com>
2022-08-03 15:30:20 -0300

    ok

---
+++ 2- java/projetos-aula-logica/src/aula27072022/Atividade10.java
@@ -0,0 +1,23 @@
+package aula27072022;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Atividade10 {
+    public static void main(String[] args) {
+        String sexo;
+        double altura;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Qual sua altura?");
+        altura = teclado.nextDouble();
+        System.out.println("Qual seu sexo?");
+        sexo = teclado.next();
+
+
+
+
+
+    }
+}
