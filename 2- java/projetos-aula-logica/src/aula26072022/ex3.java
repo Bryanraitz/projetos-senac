c5081a6f7d624e955352348ee74dd7919e09f44a
bryan.oechsler <bryanraitz598@hotmail.com>
2022-08-03 14:11:00 -0300

    ok

---
+++ 2- java/projetos-aula-logica/src/aula26072022/ex3.java
@@ -0,0 +1,11 @@
+public class ex3 {
+    public static void main(String[] args) {
+        System.out.println("Isto nao vai dar cert");
+        System.out.println("Voce nunca vai conseguir");
+        System.out.println("Voce vai se estrepar");
+        System.out.println("Nao vai dar em nada");
+        System.out.println("Esta tudo errado!");
+        System.out.println("Vai e se der medo volta");
+        System.out.println("Nao espere dar certo. faca dar errado.");
+    }
+}
---
+++ 2- java/projetos-aula-logica/src/aula26072022/ex9.java
@@ -0,0 +1,8 @@
+public class ex9 {
+    public static void main(String[] args) {
+        System.out.println("X");
+        System.out.println("X");
+        System.out.println("X");
+        System.out.println("XXXX");
+    }
+}
